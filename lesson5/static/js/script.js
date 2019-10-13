f7fb56f90cb5e5dbf269241a4c2891b673873106
splucena <splucena2011@gmail.com>
2019-10-13 18:41:10 +0800

    Fixed header news.

--- lesson5/static/js/script.js
+++ lesson5/static/js/script.js
@@ -1,19 +1,35 @@
+let date_today = new Date();
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dececember"];
+let days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let is_friday = date_today.getDay();
+
 window.onload = function () {
-    
-    let date_today = new Date();
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dececember"];
-    let days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
     document.getElementById('footer-year').innerHTML = date_today.getFullYear();
     document.getElementById('footer-date').innerHTML = days_of_the_week[date_today.getDay()] +', '+ date_today.getDate() + ' ' + months[date_today.getMonth()] + ' ' + date_today.getFullYear()
 
-    let is_friday = 5;
     let friday_banner = document.getElementById('friday-banner');
+
     if (is_friday == 5) {
         friday_banner.style.display = "block";
+        on_resize()
     }
 }
 
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+function on_resize() {
+    let header_element = document.getElementById('header-element');
+    let int_viewport_width = window.innerWidth;
+
+    if (is_friday == 5) {
+        if (int_viewport_width < 630) {
+            header_element.style.paddingTop = "70px";
+        } else if (int_viewport_width >= 630 && int_viewport_width <= 700) {
+            header_element.style.paddingTop = "50px";
+        } else {
+            header_element.style.paddingTop = "30px";
+        }
+    }
 }
