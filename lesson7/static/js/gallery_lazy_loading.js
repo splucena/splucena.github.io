5c9a8fdd89641f61960fad09885ac7a35f9eb6cb
splucena <sumilangplucena@yahoo.com.ph>
2019-11-01 13:25:26 +0800

    Fix "Element div not allowed as child of element picture" error.

--- lesson7/static/js/gallery_lazy_loading.js
+++ lesson7/static/js/gallery_lazy_loading.js
@@ -4,16 +4,16 @@ startup = () => {
     let observer_options = {
         root: null,
         rootMargin: '0px',
-        threshold: [0.0, 1.0] 
+        threshold: [0.0, 1.0]
     };
 
     let gallery_images = document.getElementById('gallery-images');
-    for (let i=0; i<gallery_images.childNodes.length; i++) {
+    for (let i = 0; i < gallery_images.childNodes.length; i++) {
         let img = gallery_images.childNodes[i];
-        
-        if (img.nodeType == 1) {
-            let img_id = 'img-' + (i+1);
-            img.childNodes[1].id = img_id;
+
+        if (img.tagName == 'DIV') {
+            let img_id = 'img-' + (i + 1);
+            img.childNodes[1].childNodes[1].id = img_id;
             observers[i] = new IntersectionObserver(intersectionCallback, observer_options);
             observers[i].observe(document.querySelector('#' + img_id));
         }
@@ -27,8 +27,8 @@ intersectionCallback = (entries) => {
         box.innerHTML = visible_pct + ' ' + box.id;
         if ((entry.intersectionRatio * 100) >= 100) {
             entry.target.setAttribute('src', entry.target.getAttribute('data-src'));
-        } 
+        }
     });
-}  
+}
 
 startup();
