64c5ec84c58ec0331d2ae6467bf699d420bb1af1
splucena <sumilangplucena@yahoo.com.ph>
2019-11-07 12:41:13 +0800

    Added town data.

--- lesson7/static/js/gallery_lazy_loading.js
+++ lesson7/static/js/gallery_lazy_loading.js
@@ -1,34 +1,37 @@
 let observers = [];
 
 startup = () => {
-    let observer_options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: [0.0, 1.0]
-    };
+  let observer_options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: [0.0, 1.0]
+  };
 
-    let gallery_images = document.getElementById('gallery-images');
-    for (let i = 0; i < gallery_images.childNodes.length; i++) {
-        let img = gallery_images.childNodes[i];
+  let gallery_images = document.getElementById("gallery-images");
+  for (let i = 0; i < gallery_images.childNodes.length; i++) {
+    let img = gallery_images.childNodes[i];
 
-        if (img.tagName == 'DIV') {
-            let img_id = 'img-' + (i + 1);
-            img.childNodes[1].childNodes[1].id = img_id;
-            observers[i] = new IntersectionObserver(intersectionCallback, observer_options);
-            observers[i].observe(document.querySelector('#' + img_id));
-        }
+    if (img.tagName == "DIV") {
+      let img_id = "img-" + (i + 1);
+      img.childNodes[1].childNodes[1].id = img_id;
+      observers[i] = new IntersectionObserver(
+        intersectionCallback,
+        observer_options
+      );
+      observers[i].observe(document.querySelector("#" + img_id));
     }
-}
+  }
+};
 
-intersectionCallback = (entries) => {
-    entries.forEach((entry) => {
-        let box = entry.target;
-        let visible_pct = (Math.floor(entry.intersectionRatio * 100)) + '%';
-        box.innerHTML = visible_pct + ' ' + box.id;
-        if ((entry.intersectionRatio * 100) >= 100) {
-            entry.target.setAttribute('src', entry.target.getAttribute('data-src'));
-        }
-    });
-}
+intersectionCallback = entries => {
+  entries.forEach(entry => {
+    let box = entry.target;
+    let visible_pct = Math.floor(entry.intersectionRatio * 100) + "%";
+    box.innerHTML = visible_pct + " " + box.id;
+    if (entry.intersectionRatio * 100 >= 100) {
+      entry.target.setAttribute("src", entry.target.getAttribute("data-src"));
+    }
+  });
+};
 
 startup();
---
+++ lesson9/static/js/town_data.js
@@ -0,0 +1,47 @@
+const request_url =
+    "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(request_url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json_object) {
+        console.table(json_object);
+        console.log(Object.keys(json_object['towns'][0]));
+
+        //["name", "photo", "motto", "yearFounded", "currentPopulation", "averageRainfall", "events"]
+
+        const towns = json_object["towns"];
+        for (let i = 0; i < towns.length; i++) {
+            let card = document.createElement("section");
+            let h2 = document.createElement("h2");
+
+            let town_name = towns[i].name;
+            h2.textContent = town_name;
+            card.appendChild(h2);
+
+            let motto = document.createElement("p");
+            motto.textContent = towns[i].motto;
+            card.appendChild(motto);
+
+            let year_founded = document.createElement("p");
+            year_founded.textContent = "Year Founded: " + towns[i].yearFounded;
+            card.appendChild(year_founded);
+
+            let population = document.createElement("p");
+            population.textContent = "Population: " + towns[i].population;
+            card.appendChild(population);
+
+            let average_rainfall = document.createElement("p");
+            average_rainfall.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+            card.appendChild(average_rainfall);
+
+            //let img = document.createElement("img");
+            //img.setAttribute("src", towns[i].photo);
+            //let order = towns[i].order;
+            //img.setAttribute("alt", town_name);
+            //card.appendChild(img);
+
+            document.querySelector("div.cards").appendChild(card);
+        }
+    });
