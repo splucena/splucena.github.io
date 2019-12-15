fbbc451545d54ede6590dfefe7a0b88478ea94e4
splucena <sumilangplucena@yahoo.com.ph>
2019-12-16 00:46:24 +0800

    Added popup and resource page.

--- termproject/static/js/script.js
+++ termproject/static/js/script.js
@@ -11,15 +11,46 @@ function toggleMenu() {
     document.getElementsByClassName("nav")[0].classList.toggle("responsive");
 }
 
-function validate_form() {
+function displayPopup() {
+    var w = 400;
+    var h = 400;
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var systemZoom = width / window.screen.availWidth;
+    var left = (width - w) / 2 / systemZoom + dualScreenLeft
+    var top = (height - h) / 2 / systemZoom + dualScreenTop
+    window.open('thanks.html', '_blank', 'scrollbars=yes, width=' + w / systemZoom + ', height=' + h / systemZoom + ', top=' + top + ', left=' + left);
+}
+
+/*var frm_reservation = document.getElementById('frm-reservation');
+frm_reservation.onsubmit = function () {
+    displayPopup();
+}
+
+var frm_contact_us = document.getElementById('frm-contact-us');
+frm_contact_us.onsubmit = function () {
+    displayPopup();
+}*/
+
+function validate_form(formID) {
     let full_name, phone_number, zip;
     full_name = document.getElementById('fullname');
     phone_number = document.getElementById('phone_number');
     zip = document.getElementById('zip');
 
     if (full_name.checkValidity() && phone_number.checkValidity() && zip.checkValidity()) {
-        let win = window.open('thanks.html', '_blank');
-        win.focus();
+        /*let win = window.open('thanks.html', '_blank');
+        win.focus();*/
+        //console.log(1);
+        //return displayPopup(btnID);
+        var frm = document.getElementById(formID);
+        frm.onsubmit = function () {
+            displayPopup();
+        }
     }
 }
 
